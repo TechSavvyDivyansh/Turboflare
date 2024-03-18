ddb6ecf6751d59531b562d614096e69f62e76d24
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2024-03-18 20:47:32 +0530

    creating user model and a test api endpoint

---
+++ api/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+
+
+
+const User=mongoose.model('User',userSchema)
+
+export default User 
