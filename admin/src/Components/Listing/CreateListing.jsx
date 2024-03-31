c0980cbcffe866d795d367417ab3a4e259a65f3a
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2024-03-31 18:37:57 +0530

    image upload and popup functionality completed with data collection from forms

--- admin/src/Components/Listing/CreateListing.jsx
+++ admin/src/Components/Listing/CreateListing.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CreateVariantPopup from './CreateVariantPopup'
+import {getStorage,getDownloadURL,ref,uploadBytesResumable} from 'firebase/storage'
+import {app} from '../../firebase.js'
 
 export default function CreateListing() {
 
   let [openPopup,setOpenPopup]=useState(false)
-  let [carData,setCarData]=useState({})
-  console.log(carData);
+  let [variantData,setVariantData]=useState([])
+
+  let [carData,setCarData]=useState({
+    "carNo":0,
+    "carName":"",
+    "carDesc":"",
+    "carType":"",
+    "driveType":"",
+    "claimMileage":0,
+    "horsePower":0,
+    "imageUrls":[],
+    "variants":[]
+  })
+  let [Files,setFiles]=useState([])
+  
+  useEffect(() => {
+    setCarData({ ...carData, variants: variantData });
+  }, [variantData]);
+  
+  
 
   let handleChange=(e)=>{
       setCarData({
@@ -14,72 +34,133 @@ export default function CreateListing() {
       })
   }
 
-  return (
-      <div className="flex justify-center p-10 gap-10 w-[85vw] h-[100vh] mt-10">
-            <div className="left flex flex-col gap-5 w-[25vw]">
-                  <input
-                    type="text"
-                    className="p-3 rounded bg-[#2C2A2A] w-full cursor-auto focus:outline-none"
-                    placeholder="car name"
-                    id='carName'
-                    onChange={handleChange}
-                    required
-                  />
-                  <textarea
-                    name=""
-                    id="carDesc"
-                    cols="30"
-                    rows="10"
-                    className="p-3 rounded bg-[#2C2A2A] w-full cursor-auto focus:outline-none resize-none"
-                    placeholder="CAR DESCRIPTION"
-                    onChange={handleChange}
-                    required
-                  ></textarea>
-                  <select className='p-3 rounded bg-[#2C2A2A] w-full cursor-pointer focus:outline-none' name="" id="carType" defaultValue="" onChange={handleChange} required>
-                    <option disabled value="">--Select Car type--</option>
-                    <option>SUV</option>
-                    <option>Luxury</option>
-                    <option>Sports Coupe</option>
-                    <option>Hatchback</option>
-                    <option>Sedan</option>
-                    <option>Compact/Subcompact Suv</option>
-                    <option>MUV</option>
-                    <option>Convertible</option>
-                  </select>
-                  <select className='p-3 rounded bg-[#2C2A2A] w-full cursor-pointer focus:outline-none' name="" id="driveType" defaultValue="" onChange={handleChange} required>
-                    <option disabled value="">--Select Drive type--</option>
-                    <option>FWD</option>
-                    <option>RWD</option>
-                    <option>AWD</option>
-                  </select>
-                  <input
-                    type="text"
-                    className="p-3 rounded bg-[#2C2A2A] w-full cursor-auto focus:outline-none"
-                    placeholder="claim mileage"
-                    id='claimMileage'
-                    onChange={handleChange}
-                    required
-                  />
-                  <input
-                    type="text"
-                    className="p-3 rounded bg-[#2C2A2A] w-full cursor-auto focus:outline-none"
-                    placeholder="HorsePower"
-                    id='horsePower'
-                    onChange={handleChange}
-                    required
-                  />
-                  <button onClick={()=>{setOpenPopup(true)}} className='p-3 rounded border border-[#959292] text-[#959292] text-center w-full'>ADD VARIANT</button>
-            </div>
 
-            <div className="right flex flex-col items-center gap-10">
-                <p>CAR IMAGES <span className="text-[#4d4c4c] p-3">The first image will be the cover(min 3 and max 4)</span></p>
-                <div className="flex gap-2 w-full">
-                    <input type="file" name="" id="" className="cursor-pointer border p-3 rounded w-full border-[#353434]" required accept='image/*' multiple/>
-                    <button className='p-3 border border-green-700 text-green-700 rounded'>UPLOAD</button>
-                </div>
-                <button className="border border-[#959292] text-[#959292] p-3 rounded w-full">CREATE LISTING</button>
+  const handleImgSubmit=async()=>{
+
+        if(Files.length>=2 && File.length+carData.imageUrls.length<5)
+        {
+           try {
+            
+                const urls=[]
+                for (let i = 0; i < Files.length; i++) {
+                  const url = await storeImg(Files[i]);
+                  urls.push(url);
+                }
+
+                setCarData({...carData,imageUrls:carData.imageUrls.concat(urls)})
+
+           } catch (error) {
+                console.log("image upload failed ",error);
+           }
+
+        }
+
+  }
+
+
+  const storeImg=async(file)=>{
+
+      return new Promise((resolve, reject) => {
+          const storage=getStorage(app)
+          const fileName=new Date().getTime()+file.name  
+          const storageRef=ref(storage,fileName)
+
+
+          const uploadTask=uploadBytesResumable(storageRef,file)
+
+          uploadTask.on("state_changed",(snapshot)=>{
+            const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100
+            console.log(`uploaded bytes is ${progress}%`)
+          },(err)=>{
+            reject(err)
+          },()=>{
+            getDownloadURL(uploadTask.snapshot.ref).then((firebaseUrl)=>{
+                        resolve(firebaseUrl)
+                  })
+          })
+
+      })
+
+  }
+
+  return (
+      <div className="flex flex-col items-center p-10 gap-10 w-[85vw] h-[100vh] mt-10">
+            <div className="flex gap-10 w-fit mx-auto">
+                    <form className="left flex flex-col gap-5 w-[25vw]">
+                          <input
+                            type="text"
+                            className="p-3 rounded bg-[#2C2A2A] w-full cursor-auto focus:outline-none"
+                            placeholder="car name"
+                            id='carName'
+                            onChange={handleChange}
+                            required
+                          />
+                          <textarea
+                            name=""
+                            id="carDesc"
+                            cols="30"
+                            rows="10"
+                            className="p-3 rounded bg-[#2C2A2A] w-full cursor-auto focus:outline-none resize-none"
+                            placeholder="CAR DESCRIPTION"
+                            onChange={handleChange}
+                            required
+                          ></textarea>
+                          <select className='p-3 rounded bg-[#2C2A2A] w-full cursor-pointer focus:outline-none' name="" id="carType" defaultValue="" onChange={handleChange} required>
+                            <option disabled value="">--Select Car type--</option>
+                            <option>SUV</option>
+                            <option>Luxury</option>
+                            <option>Sports Coupe</option>
+                            <option>Hatchback</option>
+                            <option>Sedan</option>
+                            <option>Compact/Subcompact Suv</option>
+                            <option>MUV</option>
+                            <option>Convertible</option>
+                          </select>
+                          <select className='p-3 rounded bg-[#2C2A2A] w-full cursor-pointer focus:outline-none' name="" id="driveType" defaultValue="" onChange={handleChange} required>
+                            <option disabled value="">--Select Drive type--</option>
+                            <option>FWD</option>
+                            <option>RWD</option>
+                            <option>AWD</option>
+                          </select>
+                          <input
+                            type="text"
+                            className="p-3 rounded bg-[#2C2A2A] w-full cursor-auto focus:outline-none"
+                            placeholder="claim mileage"
+                            id='claimMileage'
+                            onChange={handleChange}
+                            required
+                          />
+                          <input
+                            type="text"
+                            className="p-3 rounded bg-[#2C2A2A] w-full cursor-auto focus:outline-none"
+                            placeholder="HorsePower"
+                            id='horsePower'
+                            onChange={handleChange}
+                            required
+                          />
+                          
+                    </form>
+
+                    <div className="right flex flex-col items-center gap-10">
+                        <p>CAR IMAGES <span className="text-[#4d4c4c] p-3">The first image will be the cover(min 2 and max 5)</span></p>
+                        <div className="flex gap-2 w-full">
+                            <input onChange={(e)=>setFiles(e.target.files)} type="file" name="" id="" className="cursor-pointer border p-3 rounded w-full border-[#353434]" required accept='image/*' multiple/>
+                            <button onClick={handleImgSubmit} className='p-3 border border-green-700 text-green-700 rounded'>UPLOAD</button>
+                        </div>  
+                        {carData.imageUrls.length>0 && carData.imageUrls.map((img,index)=>{
+                            return <div className='flex justify-between p-1 border items-center w-full' key={index}>
+                                        <img src={img} alt="listing img" className='w-28 h-28 object-contain rounded-lg'/>
+                                        {/* <button type='button' onClick={()=>{handleRemoveImg(index)}} className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'>Delete</button> */}
+                                  </div>
+                        })}                     
+                        
+                    </div>
             </div>
-            {openPopup && <CreateVariantPopup setOpenPopup={setOpenPopup}/>}
+            {openPopup && <CreateVariantPopup setOpenPopup={setOpenPopup} setVariantData={setVariantData} variantData={variantData}/>}
+            <div className="flex flex-col gap-5 ">
+                  <button onClick={()=>{setOpenPopup(true)}} className='p-3 rounded border border-[#959292] text-[#959292] text-center'>ADD VARIANT</button>
+                  <button className='p-3 rounded border border-[#959292] text-[#959292] text-center'>CREATE LISTING</button>
+            </div>
 
       </div>
       
--- admin/src/Components/Listing/CreateVariantPopup.jsx
+++ admin/src/Components/Listing/CreateVariantPopup.jsx
@@ -1,49 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function CreateVariantPopup(props) {
+  let [data,setData]=useState({
+      "variantName":"",
+      "price":0,
+      "adas":false,
+      "adrenox":false,
+      "manual":false,
+      "automatic":false,
+      "fuel":""
+  })
+  console.log(data);
+
+  let handleVariantChange=(e)=>{
+      if(e.target.type==="checkbox")
+      {
+          setData({...data,[e.target.id]:e.target.checked})
+      }
+      else
+      {
+        setData({...data,[e.target.id]:e.target.value})
+      }
+  }
+
+  let handleCreateVariant=(e)=>{
+        e.preventDefault()
+        props.setVariantData([...props.variantData,data])
+        props.setOpenPopup(false)
+  }
+
   return (
     <div className=' absolute top-0 left-0 w-[100vw] h-[100vh] flex items-center justify-center bg-[#3432326c] backdrop-blur'>
-        <div className="variant-popup bg-[#373232] p-14 flex flex-col items-center gap-8 rounded shadow-2xl">
-                                <p onClick={()=>{props.setOpenPopup(false)}} className='absolute translate-x-[13vw] -translate-y-11 cursor-pointer'><i class='bx bxl-xing text-2xl'></i></p>
+        <form onSubmit={handleCreateVariant} className="variant-popup bg-[#373232] p-14 flex flex-col items-center gap-8 rounded shadow-2xl">
+                                <p onClick={()=>{props.setOpenPopup(false)}} className='absolute translate-x-[13vw] -translate-y-11 cursor-pointer'><i className='bx bxl-xing text-2xl'></i></p>
                                 <p className="text-white">Create new variant</p>
                                 <input
                                   type="text"
                                   className="p-3 bg-[#161616] w-96 cursor-auto focus:outline-none rounded"
                                   placeholder="VARIANT NAME"
+                                  onChange={handleVariantChange}
+                                  id='variantName'
+                                  required
                                 />
                                 <input
-                                  type="text"
+                                  type="number"
                                   className="p-3 bg-[#161616] w-96 cursor-auto focus:outline-none rounded"
                                   placeholder="PRICE"
+                                  onChange={handleVariantChange}
+                                  id='price'
+                                  required
                                 />
                                 <div className="checkboxes flex gap-5">
                                       <div className="flex gap-3">
-                                          <input type="checkbox" id="" className="w-5 cursor-pointer"/>
+                                          <input type="checkbox" id="adas" className="w-5 cursor-pointer" onChange={handleVariantChange}/>
                                           <span className="text-white">ADAS</span>
                                       </div>
                                       <div className="flex gap-3">
-                                          <input type="checkbox" id="" className="w-5 cursor-pointer"/>
+                                          <input type="checkbox" id="adrenox" className="w-5 cursor-pointer" onChange={handleVariantChange}/>
                                           <span className="text-white">ADRENOX</span>
                                       </div>
                                       <div className="flex gap-3">
-                                          <input type="checkbox" id="" className="w-5 cursor-pointer"/>
+                                          <input type="checkbox" id="manual" className="w-5 cursor-pointer" onChange={handleVariantChange}/>
                                           <span className="text-white">MANUAL</span>
                                       </div>
                                       <div className="flex gap-3">
-                                          <input type="checkbox" id="" className="w-5 cursor-pointer"/>
+                                          <input type="checkbox" id="automatic" className="w-5 cursor-pointer" onChange={handleVariantChange}/>
                                           <span className="text-white">AUTOMATIC</span>
                                       </div>
                                 </div>
                                 <div className="">
-                                    <select className="bg-black cursor-pointer focus:outline-none  p-2 rounded">
-                                        <option value="">Petrol</option>
-                                        <option value="">DIESEL</option>
-                                        <option value="">ELECTRIC/HYBRID</option>
+                                    <select className="bg-black cursor-pointer focus:outline-none px-5 p-2 rounded" defaultValue="" onChange={handleVariantChange} id="fuel" required>
+                                        <option value="" disabled>--Select Fuel type--</option>
+                                        <option>Petrol</option>
+                                        <option>DIESEL</option>
+                                        <option>ELECTRIC/HYBRID</option>
                                     </select>
 
                                 </div>
                                 <button className='p-3 bg-black w-96 rounded'>CREATE VARIANT</button>
-                        </div>
+                        </form>
     </div>
   )
 }
