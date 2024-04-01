6b9c2fcccafa98929c6a514df0f8a265829a8583
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2024-04-01 23:14:43 +0530

    popup styling

--- admin/src/Components/Listing/ViewListingPopup.jsx
+++ admin/src/Components/Listing/ViewListingPopup.jsx
@@ -3,24 +3,40 @@ import React from 'react'
 export default function ViewListingPopup(props) {
   return (
     <div className='w-[100vw] h-[100vh] absolute top-0 left-0 bg-[#413f3f94] backdrop-blur flex justify-center items-center'>
-        <div className="flex flex-col items-end w-[70vw] h-[80vh] bg-[#000000] shadow-2xl overflow-y-scroll">
+        <div className="flex flex-col items-end w-[70vw] h-[80vh] bg-[#000000] shadow-2xl rounded overflow-y-scroll">
             <p onClick={()=>{props.setOpenPopup(false)}} className='cursor-pointer fixed p-3 z-10'><i className='bx bxl-xing text-2xl'></i></p>
 
-            <div className="">
-                        <div className="">
-                                <img src={props.carData.imageUrls[0]} alt="" className='w-[100%] h-[80vh] object-cover opacity-[20%]' />
-                                <div className="">
-                                        <p>{props.carData.carName}</p>
-                                        <p>{props.carData.carDesc}</p>
-                                        <p>{props.carData.claimMileage}</p>
-                                        <p>{props.carData.horsePower}</p>
-                                        <p>{props.carData.carType}</p>
-                                        <p>{props.carData.driveType}</p>
-                                        <div className="">
+            <div className="relative w-[100%]">
+                    <img src={props.carData.imageUrls[0]} alt="" className='w-[100%] h-[80vh] object-cover brightness-75' />
+                    <div className="absolute top-0 left-0 h-[100%] flex justify-between">
+                            <div className="flex flex-col gap-8 w-[50%] bg-[#00000057] backdrop-blur-[8px] p-10">
+                                        <div className="flex flex-col gap-5">
+                                            <p className='text-center text-5xl font-medium uppercase'>{props.carData.carName}</p>
+                                            <p className='w-[100%] text-center text-[#c6c5c5] font-medium line-clamp-3'>{props.carData.carDesc}</p>
+                                            <div className="flex  justify-between bg-gradient-to-r from-[#1E1D1D] via-[#1E1D1D] to-transparent p-5 py-9 text-center border border-[#999595a2] rounded">
+                                                <div className="">
+                                                    <p className='font-semibold'>{props.carData.claimMileage}</p>
+                                                    <p className='text-[#636161]'>KMPH</p>
+                                                </div>
+                                                <div className="">
+                                                    <p className='font-semibold'>{props.carData.horsePower}</p>
+                                                    <p className='text-[#636161]'>HP</p>
+                                                </div>
+                                                <div className="">
+                                                    <p className='font-semibold'>{props.carData.carType}</p>
+                                                    <p className='text-[#636161]'>(CAR TYPE)</p>
+                                                </div>
+                                                <div className="">
+                                                    <p className='font-semibold'>{props.carData.driveType}</p>
+                                                    <p className='text-[#969292]'>(DRIVE TYPE)</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className=" flex justify-between bg-gradient-to-r from-[#1E1D1D] via-[#1E1D1D] to-transparent border border-[#999595a2] p-5 rounded">
                                             {props.carData.variants.map((variant,i)=>{
                                                 return <div className="">
                                                             <p>{variant.variantName}</p>
-                                                            <p>{variant.price}</p>
+                                                            <p>₹{variant.price.toLocaleString()}</p>
                                                             <p>{variant.adas?"ADAS":""}</p>
                                                             <p>{variant.adrenox?"ADRENOX":""}</p>
                                                             <p>{variant.manual?"MANUAL":""}</p>
@@ -28,14 +44,15 @@ export default function ViewListingPopup(props) {
                                                     </div>
                                             })}
                                         </div>
-                                        <div className="">
-                                            {props.carData.imageUrls.map((img,i)=>{
-                                                return <img key={i} src={img} alt="" />
-                                            })}
-                                        </div>
-                                </div>
-                           </div>
+                            </div>
+                            <div className="p-10 w-[50%] flex flex-col gap-4 justify-center items-end overflow-y-auto">
+                                {props.carData.imageUrls.map((img,i)=>{
+                                    return <img key={i} src={img} alt="" className='w-[40%] brightness-125 h-[150px] object-cover shadow-2xl rounded'/>
+                                })}
+                            </div>
+                    </div>
             </div>
+
 
         </div>
     </div>
